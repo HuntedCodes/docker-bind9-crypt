fc7fc71c7e007534ec174f9f2567b9dc35839517
Jack Sullivan <jack@hunted.codes>
2017-05-29 09:18:06 +0000

    Runs dnscrypt by default

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,29 @@
-FROM debian
+FROM debian:stretch
 MAINTAINER Jack Sullivan
 
-# Install squid-deb-proxy
+# Install packages
 RUN apt-get update -y && \
     apt-get install -y --no-install-recommends \
-      bind9 && \
+      bind9 \
+      dnscrypt-proxy \
+      dnscrypt-proxy-plugins && \
     apt-get clean
+
+# Diagnostic packages
+RUN apt-get install -y procps dnsutils
 
 # Copy init script
 COPY entrypoint.sh /data/entrypoint.sh
 RUN chmod +x /data/entrypoint.sh
 
-# Links and paths
+# BIND
 RUN ln -sf /data/bind/etc/named.conf /etc/bind/named.conf
 RUN mkdir -p /var/run/named && chown bind:bind /var/run/named
+
+# DNSCrypt user
+RUN adduser --system --quiet \
+  --home /data/dnscrypt --shell /bin/false --group \
+  --disabled-password --disabled-login dnscrypt
 
 EXPOSE 53/udp
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
-# Ensure presence.
-#mkdir -p /data/bind/run
+if [ -z $DNS_FORWARDER ]; then DNS_FORWARDER="none"; fi
 
-# Ensure permissions.
-#chown root:root /data/bind/etc/* 
+if [ "$DNS_FORWARDER" = "dnscrypt" ]; then
+  # DNSCrypt resolver list and config
+  echo "[*] Updating DNSCrypt Resolver List"
+  wget \
+    -O /data/dnscrypt/dnscrypt-resolvers.csv \
+    https://raw.githubusercontent.com/jedisct1/dnscrypt-proxy/master/dnscrypt-resolvers.csv
+  chown dnscrypt /data/dnscrypt/dnscrypt-resolvers.csv
+  chown dnscrypt /data/dnscrypt/dnscrypt-proxy.conf
 
-# Start the bind.
-/usr/sbin/named -u bind -c /data/bind/etc/named.conf -g
+  # Run DNSCrypt
+  echo "[*] Starting DNSCrypt Proxy"
+  /usr/sbin/dnscrypt-proxy /data/dnscrypt/dnscrypt-proxy.conf;
+fi;
+
+# Link DNSCrypt and BIND config
+echo "[*] Forwarding DNS to: [$DNS_FORWARDER]"
+ln -sf \
+  "/data/bind/etc/named.conf.forwarders.$DNS_FORWARDER" \
+  "/data/bind/etc/named.conf.forwarders"
+
+# Start BIND
+echo "[*] Starting BIND Nameserver"
+/usr/sbin/named -u bind -c /data/bind/etc/named.conf -f
