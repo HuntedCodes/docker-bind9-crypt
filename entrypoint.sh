6fbc5f2bbe443c66cd6a8ac319808c92c9a3e5ef
Jack Sullivan <jack@hunted.codes>
2017-05-30 17:35:18 +0000

    Works

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-if [ -z $DNS_FORWARDER ]; then DNS_FORWARDER="none"; fi
+LOG_DIR="/data/bind/log";
+if [ ! -d "$LOG_DIR" ]; then mkdir -p $LOG_DIR && chown bind $LOG_DIR; fi
 
+if [ -z $DNS_FORWARDER ]; then DNS_FORWARDER="none"; fi
+if [ -z $DNS_BLACKHOLE ]; then DNS_BLACKHOLE="null"; fi
+
+# DNSCrypt config
 if [ "$DNS_FORWARDER" = "dnscrypt" ]; then
   # DNSCrypt resolver list and config
   echo "[*] Updating DNSCrypt Resolver List"
@@ -16,12 +21,48 @@ if [ "$DNS_FORWARDER" = "dnscrypt" ]; then
   /usr/sbin/dnscrypt-proxy /data/dnscrypt/dnscrypt-proxy.conf;
 fi;
 
-# Link DNSCrypt and BIND config
+# Update yoyo DNS blacklist
+echo "[*] Updating yoyo DNS blacklist"
+YOYO_FILE="/data/bind/etc/blacklists/yoyo"
+wget -O- \
+  "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=bindconfig&showintro=0&mimetype=plaintext" \
+  | sed -r 's/null\.zone\.file/\/data\/bind\/etc\/enabled_blackhole/g' > $YOYO_FILE
+
+# Link forwarder
 echo "[*] Forwarding DNS to: [$DNS_FORWARDER]"
 ln -sf \
   "/data/bind/etc/forwarders/$DNS_FORWARDER" \
-  "/data/bind/etc/forwarders/enabled_forwarder"
+  "/data/bind/etc/enabled_forwarder"
+
+# Link blackhole zone
+echo "[*] Blackhole zone to: [$DNS_BLACKHOLE]"
+ln -sf \
+  "/data/bind/etc/blackhole_zones/$DNS_BLACKHOLE" \
+  "/data/bind/etc/enabled_blackhole"
+
+# Permissions
+chown -R bind /data/bind/etc
+chmod 775 /data/bind/etc
+
+chown -R bind /data/bind/etc/blacklists
+chmod 775 /data/bind/etc/blacklists
+chmod 664 /data/bind/etc/blacklists/*
+
+chown -R bind /data/bind/etc/blackhole_zones
+chmod 775 /data/bind/etc/blackhole_zones
+chmod 664 /data/bind/etc/blackhole_zones/*
+
+chown -R bind /data/bind/etc/forwarders
+chmod 775 /data/bind/etc/forwarders
+chmod 664 /data/bind/etc/forwarders/*
+
+chown -R bind /data/bind/etc/zones
+chmod 775 /data/bind/etc/zones
+chmod 664 /data/bind/etc/zones/*
+
+chmod 775 /data/bind/log
 
 # Start BIND
 echo "[*] Starting BIND Nameserver"
-/usr/sbin/named -u bind -c /data/bind/etc/named.conf -f
+/usr/sbin/named -u bind -c /data/bind/etc/named.conf -4
+tail -f /data/bind/log/default.log
